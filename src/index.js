154d85fb8b40e67f8d0ad075a3e71d18a88736bd
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-02-04 10:17:53 -0500

    assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+//add click eventListener() to CREATE NEW TASK submit btn
+const form = document.getElementById("create-task-form");
+function logSubmit(event) {
+  console.log("submit clicked");
+  event.preventDefault();
+  createNewToDo();
+  //append list to list id = "task"
+}
+
+function createNewToDo() {
+  console.log("createNewTodo");
+  let node = document.createElement("li");
+  let text = document.getElementById("new-task-description").value;
+  let textNode = document.createTextNode(text);
+  node.appendChild(textNode);
+  document.getElementById("tasks").appendChild(node);
+}
+form.addEventListener("submit", logSubmit);
